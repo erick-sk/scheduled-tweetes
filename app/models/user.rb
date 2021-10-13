764335edf0ecc5528e6f512a97e289f66d2a96fa
Erick Saavedra <esvdrah@gmail.com>
2021-10-13 16:17:41 -0500

    Add twitter account model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
-class User < ApplicationRecord
   # email:string
   # password_digest:string
   #
   # passwords:string virtual
   # password_confirmation:string virtual
+
+class User < ApplicationRecord
+  has_many :twitter_accounts
 
   has_secure_password
 
