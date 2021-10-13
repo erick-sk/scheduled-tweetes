a4e9f9ae0faef0b9585c65864164f803c6e61af1
Erick Saavedra <esvdrah@gmail.com>
2021-10-13 11:44:42 -0500

    Add omniauth-twitter

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,9 @@ Rails.application.routes.draw do
   get 'password/reset/edit', to: 'password_resets#edit'
   patch 'password/reset/edit', to: 'password_resets#update'
 
+  # omniauth
+  get '/auth/twitter/callback', to: 'omniauth_callbacks#twitter'
+
   # GET /
   root to: 'main#index'
 
